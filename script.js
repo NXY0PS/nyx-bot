0121d0feeb8e4399ea0d4406a8681bfe2fe89dda
NXY0PS <emmyke@icloud.com>
2026-09-07 03:59:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const links = document.querySelectorAll('a[href^="#"]');
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    const id = link.getAttribute('href');
+    if (!id || id === '#') return;
+    const target = document.querySelector(id);
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  });
+});
